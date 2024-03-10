8587792cf23b8f11036b393c35f4473c2b024ee2
pronajitdey <deypronajit@gmail.com>
2024-03-10 10:26:37 +0530

    Changed sameSite to none for cookies

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateTokenAndSetCookie = (username, email, res) => {
   res.cookie("jwt", token, {
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development"
+    sameSite: "none",
+    secure: true
   });
 }
 
